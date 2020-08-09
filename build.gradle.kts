9930e21a0e599449b6ea5e2e6464f33406ffed00
lennykey <lennykey@gmx.de>
2020-08-09 18:23:13 +0200

    Gradle: adding ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 group = "de.learn.kotlin"
 version = "1.0-SNAPSHOT"
 
+val ktlint by configurations.creating
+
 plugins {
     id("java")
     kotlin("jvm") version "1.3.72"
@@ -15,7 +17,32 @@ var junitVersion = "5.3.2"
 dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
+    ktlint("com.pinterest:ktlint:0.37.2")
     testCompile(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
     testCompile(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
     testCompile(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
 }
+
+val outputDir = "${project.buildDir}/reports/ktlint/"
+val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
+
+val ktlintCheck by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("src/**/*.kt")
+}
+
+val ktlintFormat by tasks.creating(JavaExec::class) {
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
+
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args = listOf("-F", "src/**/*.kt")
+}
+
