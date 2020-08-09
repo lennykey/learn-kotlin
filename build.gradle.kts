f683329ad57ef088d1463578fee1f9f727390631
lennykey <lennykey@gmx.de>
2020-08-09 16:14:12 +0200

    build.gradle: upgrade to version kotlin 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 group = "de.learn.kotlin"
 version = "1.0-SNAPSHOT"
 
-plugins{
+plugins {
     id("java")
-    kotlin("jvm") version "1.3.11"
+    kotlin("jvm") version "1.3.72"
 }
 
-repositories{
+repositories {
     mavenCentral()
 }
 
 var junitVersion = "5.3.2"
 
-dependencies{
+dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
-    testCompile(group="org.junit.jupiter", name = "junit-jupiter-api",version=junitVersion )
-    testCompile(group="org.junit.jupiter", name = "junit-jupiter-params",version=junitVersion )
-    testCompile(group="org.junit.jupiter", name = "junit-jupiter-engine",version=junitVersion )
+    testCompile(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
+    testCompile(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
+    testCompile(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
 }
